b9b9619ecf5301b9d53b84297b0d966c10faaaad
Daniel A. <camouwotor@gmail.com>
2025-12-31 00:54:52 +0100

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const net = std.net;
+const posix = std.posix;
+
+pub fn main() !void {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    std.debug.print("Logs from your program will appear here!\n", .{});
+
+    // TODO: Uncomment the code below to pass the first stage
+    const address = try net.Address.resolveIp("127.0.0.1", 9092);
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
+
+    _ = try listener.accept();
+}
